d48afd2713fab557d5bd2b8e7e183cccb04991a1
freudinsky <timo.fuchs94@icloud.com>
2023-09-01 10:04:59 +0200

    ..

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import NewsContainer from "./components/NewsContainer";
+import "./App.css";
+import "./components/SearchBar.css";
+
+function App() {
+  //const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="header">
+        <a href="../index.html" id="hackernews">
+          <h1 className="headline">HackerNews</h1>
+        </a>
+      </div>
+      <NewsContainer />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SortingBar.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export default function SortingBar({ data, setData }) {
+	async function sortBy(url) {
+		try {
+			const response = await axios.get(url);
+			if (response.status === 200) {
+				setData(response.data.hits);
+			} else {
+				console.error("Request was not successful.");
+			}
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	}
+
+	return (
+		<div className="sort-wrap">
+			<ul className="sorting-bar">
+				<li
+					onClick={() =>
+						sortBy("https://hn.algolia.com/api/v1/search_by_date?tags=story")
+					}
+				>
+					new
+				</li>
+				<li
+					onClick={() =>
+						sortBy(
+							`https://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=created_at_i<${(Date.now() - 86400)}`
+						)
+					}
+				>
+					past
+				</li>
+				<li
+					onClick={() =>
+						sortBy("https://hn.algolia.com/api/v1/search_by_date?tags=ask_hn")
+					}
+				>
+					ask
+				</li>
+				<li
+					onClick={() =>
+						sortBy("https://hn.algolia.com/api/v1/search_by_date?tags=show_hn")
+					}
+				>
+					show
+				</li>
+			</ul>
+		</div>
+	);
+}
